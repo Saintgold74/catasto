ba12a4bf66da9e7bde20da8d217ca13068d88a57
Marco Santoro <santoromarco@gmail.com>
2025-06-07 07:43:49 +0200

    Riorganizza file e cartelle

--- Test_f.py
+++
@@ -1,6 +0,0 @@
-# Test_fpdf.py
-try:
-    from fpdf import FPDF
-    print("FPDF è installato e importabile correttamente.")
-except ImportError:
-    print("ERRORE: FPDF NON è installato o non è importabile.")
